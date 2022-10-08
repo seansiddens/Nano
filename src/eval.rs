cadfba8429da0194238bd8cc8c7eda3fe4dd578e
Sean Siddens <seansiddens@gmail.com>
2022-10-08 15:45:14 -0700

    Unit tests for extend_env() and lookup_id()

--- src/eval.rs
+++ src/eval.rs
@@ -4,12 +4,18 @@ use crate::types::*;
 
 /// Returns the most recent binding for the variable 'id' in the HashMap
 /// representing the environment. Panics if variable not present in environment.
-fn lookup_id(id: &Id, env: &Env) -> Value {
-    if env.contains_key(id) {
-        return env[id];
+fn lookup_id(env: &Env, id: &Id) -> Value {
+    match env.get(id) {
+        Some(value) => return value.clone(),
+        None => panic!("Error! Unbound variable."),
     }
-    panic!("Error! Unbound variable {}", id);
 }
+
+fn extend_env(env: &mut Env, id: &Id, val: &Value) {
+    // TODO: Do we have to do clones here?
+    env.insert(id.clone(), val.clone());
+}
+
 
 pub fn eval(env: Env, e: Expr) -> Value {
     match (env, e) {
@@ -17,7 +23,50 @@ pub fn eval(env: Env, e: Expr) -> Value {
         (_, Expr::EInt(n)) => Value::VInt(n),
         (_, Expr::EBool(b)) => Value::VBool(b),
         (_, Expr::ENil) => Value::VNil,
-        // TOOD: Finish rest of these.
+        (env, Expr::ELam(id, e_body)) => Value::VClos(env, id, *e_body),
+        // Lookup var in env.
+
+        
         _ => Value::VNil,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn lookup_test() {
+        let mut env = Env::new();
+        env.insert("x0".to_string(), Value::VInt(0));
+        env.insert("b0".to_string(), Value::VBool(true));
+        env.insert("nil".to_string(), Value::VNil);
+
+        assert!(lookup_id(&env, &"x0".to_string()) == Value::VInt(0));
+        assert!(lookup_id(&env, &"b0".to_string()) == Value::VBool(true));
+        assert!(lookup_id(&env, &"nil".to_string()) == Value::VNil);
+    }
+
+    #[test]
+    #[should_panic]
+    fn lookup_unbound_var() {
+        let mut env = Env::new();
+        env.insert("x0".to_string(), Value::VInt(10));
+
+        let _ = lookup_id(&env, &"foo".to_string());
+    }
+
+    #[test]
+    fn env_extend_test() {
+        let mut env = Env::new();
+        env.insert("x0".to_string(), Value::VInt(0));
+        env.insert("b0".to_string(), Value::VBool(true));
+        env.insert("nil".to_string(), Value::VNil);
+
+        extend_env(&mut env, &"foo".to_string(), &Value::VInt(69));
+        assert!(lookup_id(&env, &"foo".to_string()) == Value::VInt(69));
+
+        extend_env(&mut env, &"foo".to_string(), &Value::VInt(28));
+        assert!(lookup_id(&env, &"foo".to_string()) == Value::VInt(28));
     }
 }
